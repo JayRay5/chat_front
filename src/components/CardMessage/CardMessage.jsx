b7d06aae7fcb38dba3578a428f624c99044aaa4d
Dupeyl <dupeyroux.lisa@gmail.com>
2023-05-18 18:56:36 +0200

    for pull

--- src/components/CardMessage/CardMessage.jsx
+++ src/components/CardMessage/CardMessage.jsx
@@ -6,10 +6,26 @@ import { delete_chat, get_chats } from '../../utils/api'
 
 export default function CardMessage() {
 
-  return (
-    <div class="bubble">
-        contenu
-    </div>
+    return (
+        <>
+            <div class="bubble-container">
+                <div class="bubble received">
+                    Message reçu 1
+                </div>
 
-  )
+                <div class="bubble sent">
+                    Message envoyé 1
+                </div>
+
+                <div class="bubble received">
+                    Message reçu 2
+                </div>
+
+                <div class="bubble sent">
+                    Message envoyé 2
+                </div>
+            </div>
+
+        </>
+    )
 }
